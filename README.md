fe33f4c7be0e75f674d6afe4ac72492e232be182
jkfan2 <jkfan2@iflytek.com>
2021-01-23 22:26:51 +0800

    删除注释插件与Proxy

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # comment-webpack-plugin
 一款使用webpack开发的用于打包后特定注释移除的plugin
+ ## 执行方法
+ yarn webpack
+
+ ## 运行方法
+ yarn webpack-dev-server
+ yarn webpack-dev-server --open  //自动唤起浏览器
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+class MyPlugin {
+  apply (compiler) {
+    console.log('MyPlugin 启动')
+    //emit钩子函数为webpack即将往输出目录输出文件时执行
+    compiler.hooks.emit.tap('MyPlugin', compilation => {
+      // compilation => 可以理解为此次打包的上下文
+      for (const name in compilation.assets) {
+        // console.log(name)
+        // console.log(compilation.assets[name].source())
+        if (name.endsWith('.js')) {
+          const contents = compilation.assets[name].source()   //得到文件的内容
+          const withoutComments = contents.replace(/\/\*\*+\*\//g, '')  //移除打包文件的/*****/注释
+          compilation.assets[name] = {
+            source: () => withoutComments,  //替换
+            size: () => withoutComments.length  //大小
+          }
+        }
+      }
+    })
+  }
+}
+
+module.exports = {
+  mode: 'none',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    // publicPath: 'dist/'
+  },
+  devServer: {
+    contentBase: './public',  //可以访问到public下的文件
+    proxy: {
+      '/api': {
+        // http://localhost:8080/api/users -> https://api.github.com/api/users
+        target: 'https://api.github.com',
+        // http://localhost:8080/api/users -> https://api.github.com/users
+        pathRewrite: {
+          '^/api': ''
+        },
+        // 不能使用 localhost:8080 作为请求 GitHub 的主机名
+        changeOrigin: true
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.png$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10 * 1024 // 10 KB
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    // 用于生成 index.html
+    new HtmlWebpackPlugin({
+      title: 'Webpack Plugin Sample',
+      meta: {
+        viewport: 'width=device-width'
+      },
+      template: './src/index.html'
+    }),
+    // 用于生成 about.html
+    new HtmlWebpackPlugin({
+      filename: 'about.html'
+    }),
+    //开发阶段最好不要使用这个插件
+    // new CopyWebpackPlugin([
+    //   // 'public/**'
+    //   'public'
+    // ]),
+    new MyPlugin()
+  ]
+}
